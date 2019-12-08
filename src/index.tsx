194a4febe2adac924f46a58fb99b8fc40086f65b
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-12-08 14:17:14 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ const Box = styled.div`
   border: 1px solid rgb(30%, 30%, 30%);
   border-radius: 10px;
   padding: 1em;
+  margin: 1em;
 `
 const App = () => {
   const [result, setResult] = useState<any>(null)
